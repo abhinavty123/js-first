8db1761cf64a0aac7ac8ae13a760288653c4e9d5
abhinavty123 <abhinavty1@gmail.com>
2024-08-22 10:48:35 +0000

    new update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+// constant and variable
+const accountId =144553
+//
+let accountEmail="abhinav@gmail.com"
+var accountPassword="12345"
+accountCity="greater noida"
+
+// accountId=2    is not allowed beacuse it is constant
+accountEmail="abhi@gmail.com"
+accountPassword="345678"
+accountCity="delhi"
+console.log(accountId);
+// table ek sath multiple output de sakti h
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+/* var dont use because of issue in block scope and functional scope*/
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
 console.log("tyagi")
+// variable and constant
