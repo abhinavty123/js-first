44aab4951eae05e2099feeab8f4efe791f81ec51
abhinavty123 <abhinavty1@gmail.com>
2024-08-23 07:36:11 +0000

    datatype discussion

---
+++ 01_basics/01_datatype2.js
@@ -0,0 +1,19 @@
+// primative and non-primitive
+
+
+//primitive 7type= string,number,boolean,null,undefined,symbol,BIgInt
+  //they make copy 
+
+// referance(non-primitive)
+// array,objects,functions
+
+const hero =["ironman","batman","saktiman"]
+ const myobj={
+    nam :"abhinav",
+    age: 22,
+ }
+
+ const myfun= function(){
+console.log("hello");
+
+ }
