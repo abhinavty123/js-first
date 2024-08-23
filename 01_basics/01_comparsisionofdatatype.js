ac4fcc9fb202db403d571c651087128f728b55a5
abhinavty123 <abhinavty1@gmail.com>
2024-08-23 07:09:00 +0000

    datatype conversion in js

---
+++ 01_basics/01_comparsisionofdatatype.js
@@ -0,0 +1,17 @@
+console.log("2">1);
+console.log("02">1);
+
+ console.log(null>0);
+ console.log(null==0);
+ console.log(null>=0);
+
+ console.log(undefined==0);
+ console.log(undefined<0);
+// strict check=> means in this they check also its data type
+ console.log("3"===3);
+ 
+ 
+ 
+ 
+ 
+ 
